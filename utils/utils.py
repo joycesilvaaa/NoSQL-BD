93dec23367843f86b3a1f22596535f8e0bcfde7d
Joyce Silva <142221695+joycesilvaaa@users.noreply.github.com>
2024-11-08 07:51:05 -0300

    feat: Adicionei função de listagem de usuarios e conversão para dicionario

--- utils/utils.py
+++ utils/utils.py
@@ -1,3 +1,23 @@
+from cassandra.util import OrderedMapSerializedKey
+
+def convert_ordered_map_to_dict(ordered_map):
+    if isinstance(ordered_map, OrderedMapSerializedKey):
+        return dict(ordered_map)
+    return ordered_map 
+
+def row_to_dict(row):
+    row_dict = {
+        "id": str(row.id), 
+        "compras": row.compras,
+        "cpf": row.cpf,
+        "email": row.email,
+        "enderecos": [convert_ordered_map_to_dict(endereco) for endereco in row.enderecos],
+        "favoritos": row.favoritos,
+        "nome": row.nome,
+        "senha": row.senha,
+    }
+    return row_dict
+
 def get_product(session,product_id):
     query = """
         SELECT * FROM products WHERE id = %s
@@ -24,4 +44,22 @@ def get_user(session, user_id):
         SELECT * FROM user WHERE id = %s
     """
     user = session.execute(query, (user_id,)).one()
-    return user if user else None
+    return row_to_dict(user) if user else None
+
+def get_all_users(session):
+    query = "SELECT * FROM user"
+    users = session.execute(query)  
+    return list(users)
+
+
+
+def list_users(session):
+    users = get_all_users(session)
+    if not users:
+        return None
+    for user in users:
+        user_dict = row_to_dict(user)
+        user_id = user_dict["id"]  
+        user_name = user_dict["nome"]
+        print(f"ID: {user_id}, Nome: {user_name}")
+    return users
